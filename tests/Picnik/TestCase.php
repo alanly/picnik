0475baa70eac7e5df96921190d0f62e7120d44c0
Alan Ly <me@alanly.ca>
2014-12-19 16:23:18 -0500

    Add base test case class.

---
+++ tests/Picnik/TestCase.php
@@ -0,0 +1,51 @@
+<?php namespace Picnik;
+
+use Mockery as m;
+
+/**
+ * @author  Alan Ly <hello@alan.ly>
+ */
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	protected function getClientMock()
+	{
+		$r = m::mock('Picnik\Client');
+
+		return $r;
+	}
+
+	/**
+	 * Returns a mock of the Guzzle's ResponseInterface.
+	 * @param  int      $statusCode the HTTP status code for the response
+	 * @param  StdClass $json       the stdClass object holding the JSON response
+	 * @return ResponseInterface
+	 */
+	protected function getResponseMock($statusCode, $json)
+	{
+		$r = m::mock('GuzzleHttp\Message\ResponseInterface');
+		$r->shouldReceive('getStatusCode')->andReturn($statusCode);
+		$r->shouldReceive('json')->andReturn($json);
+
+		return $r;
+	}
+
+	/**
+	 * Returns a mock of the GuzzleHttp Client, prepared for a GET request.
+	 * @param  ResponseInterface $response the response returned for the request
+	 * @return GuzzleHttp\Client
+	 */
+	protected function getGuzzleGetClientMock($response)
+	{
+		$c = m::mock('GuzzleHttp\Client');
+		$c->shouldReceive('get')->andReturn($response);
+
+		return $c;
+	}
+	
+}
