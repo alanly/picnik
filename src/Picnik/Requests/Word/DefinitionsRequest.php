ebfba46242016b112ea0b3cb5dea44a5fe3cda0f
Alan Ly <me@alanly.ca>
2014-12-19 22:33:50 -0500

    Implement word:definitions request.

--- src/Picnik/Requests/Word/DefinitionsRequest.php
+++ src/Picnik/Requests/Word/DefinitionsRequest.php
@@ -1,6 +1,91 @@
 <?php namespace Picnik\Requests\Word;
 
+use Picnik\Client;
+
 class DefinitionsRequest extends AbstractWordRequest
 {
+
+	const API_METHOD = 'definitions';
+
+	/**
+	 * The maximum number of definitions that should be returned for the request.
+	 * This method is chainable.
+	 * @param  int $number limit the number of results to this amount
+	 * @return DefinitionsRequest
+	 */
+	public function limit($number)
+	{
+		$limit = intval($number);
+		$this->setParameter('limit', $limit);
+		return $this;
+	}
+
+	/**
+	 * Defines the usage of the word. Refer to API documentation for greater
+	 * detail and available choices. This method is chainable.
+	 * @param  string $value a part-of-speech type
+	 * @return DefinitionsRequest
+	 */
+	public function partOfSpeech($value)
+	{
+		$this->setParameter('partOfSpeech', $value);
+		return $this;
+	}
+
+	/**
+	 * Include a selection of related words with the requested definition. This
+	 * method is chainable.
+	 * @param  boolean $include to include related words or not
+	 * @return DefinitionsRequest
+	 */
+	public function includeRelated($include = true)
+	{
+		$this->setParameter('includeRelated', ($include ? true : false));
+		return $this;
+	}
+
+	/**
+	 * The source dictionaries to retrieve the definition(s) from. Sources may
+	 * either be a single option, a comma-delimited list of options, or an array
+	 * of options. Please refer to the associated API documentation for greater
+	 * detail and available choices. This method is chainable.
+	 * @param  mixed $sources the sources to use
+	 * @return DefinitionsRequest
+	 */
+	public function sourceDictionaries($sources = [])
+	{
+		if (is_array($sources) && count($sources) < 1) {
+			$sources = 'all';
+		}
+
+		$this->setParameter('sourceDictionaries', $sources);
+		return $this;
+	}
+
+	/**
+	 * Adds XML tags to the definition. Please refer to API documentation for
+	 * greater details. This method is chainable.
+	 * @param  boolean $include to include XML tags in output or not
+	 * @return DefinitionsRequest
+	 */
+	public function includeTags($include = true)
+	{
+		$this->setParameter('includeTags', ($include ? true : false));
+		return $this;
+	}
+
+	/**
+	 * Generates the request target URL based on the requested word.
+	 * @return string
+	 */
+	protected function generateRequestTarget()
+	{
+		$endpoint = Client::API_ENDPOINT;
+		$baseMethod = WordRequest::API_METHOD;
+		$format = Client::RESPONSE_FORMAT;
+		$method = DefinitionsRequest::API_METHOD;
+
+		return "{$endpoint}/{$baseMethod}.{$format}/{$this->word}/{$method}";
+	}
 	
 }
---
+++ tests/Picnik/Requests/Word/DefinitionsRequestTest.php
@@ -0,0 +1,98 @@
+<?php namespace Picnik\Requests\Word;
+
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Message\Response as GuzzleResponse;
+use GuzzleHttp\Subscriber\History as GuzzleHistory;
+use GuzzleHttp\Subscriber\Mock as GuzzleMock;
+use Picnik\TestCase;
+
+class DefinitionsRequestTest extends TestCase
+{
+
+	protected function getRequestInstance($word)
+	{
+		$d = $this->getClientMock();
+		return new DefinitionsRequest($d, $word);
+	}
+
+	public function testProperRequestGenerated()
+	{
+		$gc = new GuzzleClient;
+		$gh = new GuzzleHistory;
+		$gm = new GuzzleMock([new GuzzleResponse(200)]);
+
+		$gc->getEmitter()->attach($gm);
+		$gc->getEmitter()->attach($gh);
+
+		$c = $this->getClientMock();
+		$c->shouldReceive('getApiKey')->andReturn('foobar');
+		$c->shouldReceive('getGuzzle')->andReturn($gc);
+
+		$r = new DefinitionsRequest($c, 'foobar');
+		$r->get();
+
+		$gRequest = $gh->getLastRequest();
+
+		$target = 'https://api.wordnik.com/v4/word.json/foobar/definitions';
+
+		$this->assertStringStartsWith($target, $gRequest->getUrl());
+		$this->assertEquals($r->getParameters(), $gRequest->getQuery()->toArray());
+	}
+
+	public function testLimitParameter()
+	{
+		$r = $this->getRequestInstance('foobar');
+		$r->limit(15);
+
+		$this->assertSame(15, $r->getParameters()['limit']);
+	}
+
+	public function testPartOfSpeechParameter()
+	{
+		$r = $this->getRequestInstance('foobar');
+		$r->partOfSpeech('verb');
+
+		$this->assertSame('verb', $r->getParameters()['partOfSpeech']);
+	}
+
+	public function testIncludeRelatedParameter()
+	{
+		$r = $this->getRequestInstance('foobar');
+		$r->includeRelated(true);
+
+		$this->assertSame(true, $r->getParameters()['includeRelated']);
+	}
+
+	public function testSourceDictionariesParameterWithString()
+	{
+		$r = $this->getRequestInstance('foobar');
+		$r->sourceDictionaries('foo');
+
+		$this->assertSame('foo', $r->getParameters()['sourceDictionaries']);
+	}
+
+	public function testSourceDictionariesParameterWithCommaDelimitedString()
+	{
+		$r = $this->getRequestInstance('foobar');
+		$r->sourceDictionaries('foo,bar');
+
+		$this->assertSame('foo,bar', $r->getParameters()['sourceDictionaries']);
+	}
+
+	public function testSourceDictionariesParameterWithArray()
+	{
+		$r = $this->getRequestInstance('foobar');
+		$r->sourceDictionaries(['foo', 'bar']);
+
+		$this->assertSame(['foo', 'bar'], $r->getParameters()['sourceDictionaries']);
+	}
+
+	public function testIncludeTagsParameter()
+	{
+		$r = $this->getRequestInstance('foobar');
+		$r->includeTags(true);
+
+		$this->assertSame(true, $r->getParameters()['includeTags']);
+	}
+	
+}
