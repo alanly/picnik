a76bfa7eb8ca057e491012f579682219e80405f8
Alan Ly <me@alanly.ca>
2015-01-03 16:59:14 -0500

    Update documentation.
    - Updated the documentation for the `limit` method in order to better
      reflect its behaviour.
    
    [ci skip]

--- src/Picnik/Requests/LimitTrait.php
+++ src/Picnik/Requests/LimitTrait.php
@@ -7,7 +7,7 @@ trait LimitTrait
 	 * The maximum number of pronounciations that should be returned for the
 	 * request. This method is chainable.
 	 * @param  int $count the number of results this request should be limited to
-	 * @return mixed
+	 * @return AbstractRequest
 	 */
 	public function limit($count)
 	{
